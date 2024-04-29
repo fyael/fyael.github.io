b8d126257eae9df60cfea86565cdcf45cac23c74
fyael <152308171+fyael@users.noreply.github.com>
2024-04-29 17:02:25 -0600

    Add files via upload

---
+++ assets/js/readmore.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    } 
+    
+    
+twttr.widgets.createTimeline(
+    {
+      sourceType: "profile",
+      screenName: "TwitterDev"
+      },
+    document.getElementById("container")
+    );
