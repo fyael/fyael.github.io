e23076119e17ccdd07547485290e044356f44bd5
fyael <152308171+fyael@users.noreply.github.com>
2024-04-30 11:56:33 -0600

    Add files via upload

---
+++ readmore.js
@@ -0,0 +1,50 @@
+function myFunction_Pastor() {
+    var x = document.getElementById("div_Pasor_info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction_Fernando() {
+    var x = document.getElementById("div_Fernando_info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction_Alejandro() {
+    var x = document.getElementById("div_Alejandro_info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction_Angel() {
+    var x = document.getElementById("div_Angel_info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction_Miguel() {
+    var x = document.getElementById("div_Miguel_info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction_Victor() {
+    var x = document.getElementById("div_Victor_info");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
