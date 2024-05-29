ab762852a65ea176105024a858ef94e94668fa09
fyael <152308171+fyael@users.noreply.github.com>
2024-05-29 14:14:53 -0600

    Add files via upload

---
+++ assets/API/index.js
@@ -0,0 +1,10 @@
+const { getJson } = require("serpapi");
+
+getJson({
+  api_key: "d2f9750d9d5ef29cf13008f8cb696d7725955f17dcb2dea1cdad37ff75f0ffc8",
+  engine: "google_scholar_author",
+  hl: "en",
+  author_id: "EicYvbwAAAAJ"
+}, (json) => {
+  console.log(json);
+});
